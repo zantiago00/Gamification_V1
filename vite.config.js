48855a5a626f857606af294da95089a7739a295f
zantiago00 <el.sbz.00@gmail.com>
2025-07-03 11:41:03 -0500

    Fix GitHub Pages path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/gamification/',
+  base: './',
   plugins: [react()],
   resolve: {
     alias: {
